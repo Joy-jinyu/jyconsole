9bce876f46619402ceeafc1658fe4b6246aa360e
王艺锦 <wyj01356503@antgroup.com>
2022-05-13 18:28:16 +0800

    feat: 增加断言模块

--- test/chai/assert.test.ts
+++ test/chai/assert.test.ts
@@ -2,14 +2,23 @@ import { assert } from "chai";
 import { describe, it } from "mocha";
 
 describe("测试chai的断言Api", function () {
-  it("断言Api", function () {
-    const name = "joy";
-    const userInfo = {
-      username: "wyj",
-      password: "like",
-      hobby: ["girl", "play"],
-    };
+  const name = "joy";
+  const userInfo = {
+    username: "wyj",
+    password: "like",
+    hobby: ["girl", "play"],
+  };
+
+  it("断言Api equal", function () {
     assert.equal(name, "joy", "姓名相等");
-    assert.lengthOf(userInfo.hobby, 2);
+  });
+
+  it("断言Api typeOf", function () {
+    assert.typeOf(name, "string", "姓名是个string类型");
+    assert.typeOf(userInfo, "object", "用户信息是个对象");
+  });
+
+  it("断言Api lengthOf", function () {
+    assert.lengthOf(userInfo.hobby, 2, "用户爱好长度为2");
   });
 });
